a3a0944c7f1cc2b080df6e44f40b52f37bdce982
napster <mitchell-mays>
2016-03-01 15:38:48 -0500

    steps

---
+++ Steps.py
@@ -0,0 +1,10 @@
+'''
+Step 1: Figure out how to run executable in sandbox
+Step 2: Gather information from this running executable
+Step 3: Figure out features based on behavior analysis
+Step 4: Put data in testing and training files
+Step 5: Create Neural Network for data
+Step 6: Mechanism to train/test neural network
+*-
+Step 8: Test classifier against system with running malware binaries
+'''
