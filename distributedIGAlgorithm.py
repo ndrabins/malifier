85011f120c1fb8e6b6cfcfe235f5fa73d9566989
mitchell-mays <mitchellmays95@yahoo.com>
2016-11-21 23:38:21 -0500

    working on distribution of information gain

--- distributedIGAlgorithm.py
+++ distributedIGAlgorithm.py
@@ -1,17 +1,79 @@
-import random
-import glob
-import classDict
+import sys
 import math
-import FeedForwardNeuralNetwork as NN
+import glob
+import tensorflow as tf
 
-TRAIN_FILE_PATH = "/media/napster/data/train/"  # "F:/train/"
-MALWARE_FILE_PATH = TRAIN_FILE_PATH + "train/"
+TRAIN_FILE_PATH = sys.argv[0]
+MALWARE_FILE_PATH = TRAIN_FILE_PATH + "nGramFeatures/"
 
-ASM_Files = glob.glob(MALWARE_FILE_PATH + "*.asm")
+ASM_Files = glob.glob(MALWARE_FILE_PATH + "*.txt")
 CLASS_Files = TRAIN_FILE_PATH + "trainLabels.txt"
 
+class FFNN:
+    def __init__(self, trainData, trainLabels, testData, testLabels, inputSize, outputSize):
+        self.trX = trainData
+        self.trY = trainLabels
+        self.teX = testData
+        self.teY = testLabels
+        self.iSize = inputSize
+        self.oSize = outputSize
+
+
+    def init_weights(self, shape):
+        return tf.Variable(tf.random_normal(shape, stddev=0.01))
+
+
+    def model(self, X, w_h, w_o):
+        h = tf.nn.sigmoid(tf.matmul(X, w_h)) # this is a basic mlp, think 2 stacked logistic regressions
+        return tf.matmul(h, w_o) # note that we dont take the softmax at the end because our cost fn does that for us
+
+
+    def trainAndClassify(self):
+        X = tf.placeholder("float", [None, self.iSize])
+        Y = tf.placeholder("float", [None, self.oSize])
+
+        # !!!!!!!!!!!!!!!!
+        #I probably want nowhere near 625 for finding information gain --- see if changes need to be made
+        # !!!!!!!!!!!!!!!!
+
+        w_h = self.init_weights([self.iSize, 625]) # create symbolic variables
+        w_o = self.init_weights([625, self.oSize])
+
+        py_x = self.model(X, w_h, w_o)
+
+        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(py_x, Y)) # compute costs
+        train_op = tf.train.GradientDescentOptimizer(0.05).minimize(cost) # construct an optimizer
+        predict_op = tf.argmax(py_x, 1)
+
+        # Launch the graph in a session
+        with tf.Session() as sess:
+            # you need to initialize all variables
+            tf.initialize_all_variables().run()
+
+            for i in range(500):
+                #run training
+                for batch in range(0, len(self.trX) - 50, 50):
+                    sess.run(train_op, feed_dict={X: self.trX[batch:batch+50], Y: self.trY[batch:batch+50]})
+
+
+            #show test results
+            #print(i, np.mean(np.argmax(self.teY, axis=1) ==
+            #                 sess.run(predict_op, feed_dict={X: self.teX})))
+            value = sess.run(predict_op, feed_dict={X: self.trX})
+            return value
+
+
+def getMalClasses(fName):
+    classDictionary = {}
+    file = open(fName, 'r')
+    for line in file:
+        nameAndClass = line.split()
+        classDictionary[nameAndClass[0]] = nameAndClass[1]
+
+    return classDictionary
+
 def calcSetEntropy():
-    classDictionary = classDict.getMalClasses(CLASS_Files)
+    classDictionary = getMalClasses(CLASS_Files)
 
     classes = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     for file in ASM_Files:
@@ -45,11 +107,10 @@ def avgEntropyPerItem(newClassification):
 
 
 
-    classDictionary = classDict.getMalClasses(CLASS_Files)
+    classDictionary = getMalClasses(CLASS_Files)
 
     ############################################
     ############################################
-
 
     weightedAverageEntropy = 0
     for guessedClass in range(len(guessedClasses)):
@@ -76,50 +137,6 @@ def calcInformationGain(origEntropy, newClassification):
     afterEntropy = avgEntropyPerItem(newClassification)
     return origEntropy-afterEntropy
 
-#i am going to seek to parallelize the code inside of this function
-#i will be sending out the processes to the CSE computers
-def createInformationGainTaskList():
-
-    #process ->
-    #   1. Go through all files and list all unique ngrams
-    #   2. Split list of all ngrams into tasks
-
-    doneLog = {}
-    batchSize = 5
-
-    batch = []
-
-    myTaskLogFile = open(TRAIN_FILE_PATH + "TaskLog.txt", 'a')
-
-    for fName in range(len(ASM_Files)):
-        fString = ""
-        if fName % 10 == 0:
-            print(str(fName) + "   |   " + str(len(ASM_Files)))
-
-        fileStub = ASM_Files[fName].split('/')[-1][:-4]
-        try:
-            file = open(TRAIN_FILE_PATH + "nGramFeatures/" + fileStub + ".txt", 'r')
-            nGramDict = dict(eval(file.readline()))
-            file.close()
-        except FileNotFoundError:
-            print("---- " + fileStub + " not found ----")
-            continue
-
-        for k,v in nGramDict.items():
-            if not k in doneLog:
-                #An ngram has been selected for evaluation
-                doneLog[k] = True
-                batch.append(k)
-                if len(batch) == batchSize:
-                    fString = fString + str(batch) + "\n"
-                    batch = []
-
-        myTaskLogFile.write(fString)
-
-    myTaskLogFile.close()
-
-def distributeInformationGainBackLog():
-    myTaskLogFile = open(TRAIN_FILE_PATH + "TaskLog.txt", 'r')
 
 def runProcessBacklogItem(batch):
     #a full batch of ngrams have been selected, go through files and run these ngrams through FFNN
@@ -128,7 +145,7 @@ def runProcessBacklogItem(batch):
     classMatrix = []
 
     classMatrixBase = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    classDictionary = classDict.getMalClasses(CLASS_Files)
+    classDictionary = getMalClasses(CLASS_Files)
 
     origEntropy = calcSetEntropy()
 
@@ -161,7 +178,7 @@ def runProcessBacklogItem(batch):
 
     #dataMatrix[:], classMatrix[:], file_list = zip(*combined)
     file_list = ASM_Files[:]
-    myNet = NN.FFNN(dataMatrix, classMatrix, [], [], len(batch), 9)
+    myNet = FFNN(dataMatrix, classMatrix, [], [], len(batch), 9)
     result = myNet.trainAndClassify()
     findEntropySet = []
     for item in range(len(result)):
@@ -174,5 +191,4 @@ def runProcessBacklogItem(batch):
 
     #myIGFile.close()
 
-
-createInformationGainTaskList()
+runProcessBacklogItem(list(eval(sys.argv[0])))
